b1de1164dd931d458cc03de13b5234115ea73769
Dayanand Chinchure <dchinchure@gmail.com>
2018-07-29 19:06:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
+//This is simple get call
 'use strict';
 
 const express = require('express');
 
-// Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello Samartha !\n');
