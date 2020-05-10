1bf0ceba778def4509619a86e9ac22514b194246
Dayanand Chinchure <dchinchure@gmail.com>
2020-05-10 10:14:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Dayanand Chinchure PUCSD Batch -13 is best\n');
+  res.send('Welcome to jarvis . . .\n');
 });
 
 app.listen(PORT, HOST);
